ae75922345bdf0c298a8f48cd20dbc6a9dfe3f3e
Petr Kalis <petr.kalis@ynput.io>
2023-09-07 13:52:02 +0200

    Added settings for CollectVersionControl plugin

--- server/settings/main.py
+++ server/settings/main.py
@@ -8,6 +8,49 @@ def backend_enum():
     ]
 
 
+class CollectVersionControlProfileModel(BaseSettingsModel):
+    _layout = "expanded"
+    host_names: list[str] = Field(
+        default_factory=list,
+        title="Host names",
+    )
+    product_types: list[str] = Field(
+        default_factory=list,
+        title="Families",
+    )
+    task_types: list[str] = Field(
+        default_factory=list,
+        title="Task types",
+    )
+    task_names: list[str] = Field(
+        default_factory=list,
+        title="Task names",
+    )
+    add_version_control: bool = Field(
+        True,
+        title="Add Version Control to representations",
+    )
+    template_name: str = Field("", title="Template name",
+        description="Name from Anatomy to provide path and name of "
+                    "committed file")
+
+
+class CollectVersionControlModel(BaseSettingsModel):
+    _isGroup = True
+    enabled: bool = True
+    profiles: list[CollectVersionControlProfileModel] = Field(
+        default_factory=list,
+        title="Profiles to add version control",
+    )
+
+
+class PublishPluginsModel(BaseSettingsModel):
+    CollectVersionControl: CollectVersionControlModel = Field(
+        default_factory=CollectVersionControlModel,
+        title="Collect Version Control",
+        description="Configure which products should be version controlled externally.")  # noqa
+
+
 class VersionControlSettings(BaseSettingsModel):
     """Version Control Project Settings."""
 
@@ -17,6 +60,11 @@ class VersionControlSettings(BaseSettingsModel):
         "backendName",
         enum_resolver=backend_enum,
         title="Backend name"
+    )
+
+    publish: PublishPluginsModel = Field(
+        default_factory=PublishPluginsModel,
+        title="Publish Plugins",
     )
 
 
