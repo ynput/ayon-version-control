8737489f7d69028ff797588eb5b5e9df297a8c2e
kalisp <petr.kalis@gmail.com>
2023-11-29 17:17:11 +0100

    Removed unnecessary version string
    This is not needed in latest version of Ayon server.

--- client/version_control/plugins/publish/validate_workspace.py
+++ client/version_control/plugins/publish/validate_workspace.py
@@ -23,5 +23,5 @@ class ValidateWorkspaceDir(pyblish.api.InstancePlugin):
         if not workspace_dir:
             project_name = instance.context.data.get("projectName")
             msg = ("Please provide your local folder for workspace in "
-                   "`ayon+settings://version_control:0.0.1/local_setting/workspace_dir?project={}`".format(project_name))  # noqa
+                   "`ayon+settings://version_control/local_setting/workspace_dir?project={}`".format(project_name))  # noqa
             raise PublishXmlValidationError(self, msg)
