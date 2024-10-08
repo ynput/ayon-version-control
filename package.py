31155fd4a584287789fea75e182ef74b503b3320
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-08 17:14:31 +0200

    bump version to '0.1.0+dev'

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 """Package declaring addon version."""
 name = "version_control"
 title = "Version Control"
-version = "0.1.0"
+version = "0.1.0+dev"
 client_dir = "version_control"
 
 ayon_required_addons = {
