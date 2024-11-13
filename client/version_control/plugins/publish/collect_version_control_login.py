0247b59398fe271e98419adabecded9dbf3cbb89
Petr Kalis <petr.kalis@gmail.com>
2024-11-13 19:36:57 +0100

    Use new way for conn_info, stop hard fails
    Some artists might not be working with version control at all, dont fail for them.

--- client/version_control/plugins/publish/collect_version_control_login.py
+++ client/version_control/plugins/publish/collect_version_control_login.py
@@ -9,15 +9,20 @@ Provides:
 import pyblish.api
 
 from ayon_core.addon import AddonsManager
-from ayon_core.pipeline.publish import PublishError
 from ayon_common.utils import get_local_site_id
 
 from version_control.rest.perforce.rest_stub import PerforceRestStub
 from version_control import is_version_control_enabled
+from version_control.lib import WorkspaceProfileContext
 
 
 class CollectVersionControlLogin(pyblish.api.ContextPlugin):
-    """Collect connection info and login with it."""
+    """Collect connection info and login with it.
+
+    Do not fail explicitly if version control connection info is missing.
+    Not all artists need to have credentials, not all DCCs should use
+    version control.
+    """
 
     label = "Collect Version Control Connection Info"
     order = pyblish.api.CollectorOrder + 0.4990
@@ -35,7 +40,11 @@ class CollectVersionControlLogin(pyblish.api.ContextPlugin):
             return
 
         conn_info = self._get_conn_info(
-            project_name, version_control, project_settings)
+            project_name, version_control, project_settings, context)
+
+        if not conn_info:
+            return
+
         context.data["version_control"] = conn_info
 
         PerforceRestStub.login(
@@ -46,11 +55,18 @@ class CollectVersionControlLogin(pyblish.api.ContextPlugin):
             conn_info["workspace_name"]
         )
 
-        stream = PerforceRestStub.get_stream(conn_info["workspace_name"])
+        stream = PerforceRestStub.get_stream(
+            workspace_name=conn_info["workspace_name"])
         context.data["version_control"]["stream"] = stream
         self.log.debug(f"stream::{stream}")
 
-    def _get_conn_info(self, project_name, version_control, project_settings):
+    def _get_conn_info(
+        self,
+        project_name,
+        version_control,
+        project_settings,
+        context
+    ):
         """Gets and check credentials for version-control
 
         Args:
@@ -59,34 +75,38 @@ class CollectVersionControlLogin(pyblish.api.ContextPlugin):
             project_settings (Dict[str, Any]): Prepared project settings.
 
         Returns:
-            dict[str, str]: Connection info.
-
-        Raises:
-            (PublishError): When credentials are missing.
+            dict[str, str]: Connection info or None if validation failed
         """
+        task_entity = context.data["taskEntity"]
+        workspace_context = WorkspaceProfileContext(
+            task_names=task_entity["name"],
+            task_types=task_entity["taskType"]
+        )
         conn_info = version_control.get_connection_info(
-            project_name, project_settings)
+            project_name, project_settings, workspace_context)
 
         if not all([
             conn_info["username"],
             conn_info["password"],
-            conn_info["workspace_dir"]
+            conn_info["workspace_name"]
         ]):
             site_name = get_local_site_id()
             sett_str = (
                 "ayon+settings://version_control?project="
                 f"{project_name}&site={site_name}"
             )
-            raise PublishError(
+            self.log.warning(
                 "Required credentials are missing. "
                 f"Please go to `{sett_str}` to fill it.")
+            conn_info = None
 
         if not all([conn_info["host"], conn_info["port"]]):
             sett_str = (
                 f"ayon+settings://version_control?project={project_name}"
             )
-            raise PublishError(
+            self.log.warning(
                 "Required version control settings are missing. "
                 f"Please ask your AYON admin to fill `{sett_str}`.")
+            conn_info = None
 
         return conn_info
