43233f4ee55f612fa7a87413268304c05e1fcfdf
Petr Kalis <petr.kalis@gmail.com>
2025-02-11 17:06:26 +0100

    Refactored names

--- client/ayon_perforce/plugins/publish/integrate_perforce.py
+++ client/ayon_perforce/plugins/publish/integrate_perforce.py
@@ -7,13 +7,13 @@ import pyblish.api
 
 from ayon_core.lib import StringTemplate
 
-from ayon_perforce.rest.perforce.rest_stub import (
-    PerforceRestStub
-)
+from ayon_perforce.rest.perforce.rest_stub import PerforceRestStub
 
 
 class IntegratePerforce(pyblish.api.InstancePlugin):
     """Integrate perforce items
+
+    Commits published files as a new version in Perforce
     """
 
     label = "Integrate Perforce items"
@@ -26,7 +26,7 @@ class IntegratePerforce(pyblish.api.InstancePlugin):
         version_template_key = (
             instance.data.get("perforce", {})["template_name"])
         if not version_template_key:
-            raise RuntimeError("Instance data missing 'version_control[template_name]'")   # noqa
+            raise RuntimeError("Instance data missing 'perforce[template_name]'")   # noqa
 
         if "_" in version_template_key:
             template_area, template_name = version_template_key.split("_")
@@ -39,8 +39,8 @@ class IntegratePerforce(pyblish.api.InstancePlugin):
             raise RuntimeError("Anatomy is missing configuration for '{}'".
                                format(version_template_key))
 
-        template_file_path = os.path.join(template["directory"],
-                                          template["file"])
+        template_file_path = os.path.join(
+            template["directory"], template["file"])
         anatomy_data = copy.deepcopy(instance.data["anatomyData"])
         anatomy_data["root"] = instance.data["perforce"]["roots"]
         # anatomy_data["output"] = ''
@@ -50,35 +50,38 @@ class IntegratePerforce(pyblish.api.InstancePlugin):
         for repre in instance.data["representations"]:
             anatomy_data["ext"] = repre["ext"]
 
-            version_control_path = StringTemplate.format_template(
+            perforce_file_path = StringTemplate.format_template(
                 template_file_path, anatomy_data
             )
 
             source_path = repre["published_path"]
 
-            dirname = os.path.dirname(version_control_path)
+            dirname = os.path.dirname(perforce_file_path)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
 
-            is_on_server = PerforceRestStub.exists_on_server(version_control_path)
+            is_on_server = PerforceRestStub.exists_on_server(perforce_file_path)
             actual_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            comment = os.path.basename(version_control_path) + actual_time
+            comment = os.path.basename(perforce_file_path) + actual_time
             if is_on_server:
-                if PerforceRestStub.is_checkouted(version_control_path):
-                    raise RuntimeError("{} is checkouted by someone already, "
-                                       "cannot commit right now.".format(
-                                        version_control_path))
-                if not PerforceRestStub.checkout(version_control_path,
-                                                 comment):
-                    raise ValueError("File {} not checkouted".
-                                     format(version_control_path))
+                if PerforceRestStub.is_checkouted(perforce_file_path):
+                    raise RuntimeError(
+                        f"{perforce_file_path} is checkouted by someone "
+                        f"already, cannot commit right now."
+                    )
+                if not PerforceRestStub.checkout(
+                        perforce_file_path, comment):
+                    raise ValueError(
+                        f"File {perforce_file_path} not checkouted"
+                    )
 
-            shutil.copy(source_path, version_control_path)
+            shutil.copy(source_path, perforce_file_path)
             if not is_on_server:
-                if not PerforceRestStub.add(version_control_path,
+                if not PerforceRestStub.add(perforce_file_path,
                                             comment):
-                    raise ValueError("File {} not added to changelist".
-                                     format(version_control_path))
+                    raise ValueError(
+                        f"File {perforce_file_path} not added to changelist"
+                    )
 
             if not PerforceRestStub.submit_change_list(comment):
                 raise ValueError("Changelist not submitted")
